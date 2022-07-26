5135dcc2016114f0493c43e58790ce8772b33a81
kahenteikou <kokkiemouse@gmail.com>
2022-07-27 00:09:27 +0900

    xf

--- src/main/kotlin/module-info.java
+++
@@ -1,8 +0,0 @@
-module Kotlinoder.main {
-    requires kotlin.stdlib;
-    requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.core;
-
-    exports io.github.kahenteikou.kotlinoder;
-    opens io.github.kahenteikou.kotlinoder;
-}
