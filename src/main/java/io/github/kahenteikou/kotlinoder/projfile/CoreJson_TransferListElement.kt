cb2f73c11cc64fb241258d4f58ed3c9f652f1a5b
kahenteikou <kokkiemouse@gmail.com>
2022-07-26 23:16:53 +0900

    l2

--- src/main/java/io/github/kahenteikou/kotlinoder/projfile/CoreJson_TransferListElement.kt
+++ src/main/java/io/github/kahenteikou/kotlinoder/projfile/CoreJson_TransferListElement.kt
@@ -1,4 +1,17 @@
 package io.github.kahenteikou.kotlinoder.projfile
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
+/**
+ * CoreJsonTransfer
+ */
 class CoreJson_TransferListElement {
+    /**
+     * @param name name
+     * @param value val
+     */
+    @JsonProperty("number")
+    var number:Long=0
+    @JsonProperty("name")
+    var name:String=""
 }
