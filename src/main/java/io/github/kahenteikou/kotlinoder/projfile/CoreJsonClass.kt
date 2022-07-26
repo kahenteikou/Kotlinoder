52844b246b031fde684e014186a3832a6bce0575
kahenteikou <kokkiemouse@gmail.com>
2022-07-26 23:13:41 +0900

    x2

---
+++ src/main/java/io/github/kahenteikou/kotlinoder/projfile/CoreJsonClass.kt
@@ -0,0 +1,9 @@
+package io.github.kahenteikou.kotlinoder.projfile
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class CoreJsonClass {
+    @JsonProperty("name")
+    var name: String=""
+
+}
