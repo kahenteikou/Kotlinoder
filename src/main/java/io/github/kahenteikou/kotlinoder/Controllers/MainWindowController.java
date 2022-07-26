931501d4b15c1ed4b9878b803eccefb8339329fe
kahenteikou <kokkiemouse@gmail.com>
2022-07-27 06:35:41 +0900

    x2

--- src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.java
+++ src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.java
@@ -1,13 +1,21 @@
 package io.github.kahenteikou.kotlinoder.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainWindowController  implements Initializable {
+    @FXML
+    private Button Button1;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+    }
+    @FXML
+    public void Button1_OnAction(ActionEvent event){
+        System.out.println("Button1_OnAction");
     }
 }
