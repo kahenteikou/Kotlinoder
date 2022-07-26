5357afc83f5e7db680126a14fbdb280e34d8319e
kahenteikou <kokkiemouse@gmail.com>
2022-07-26 23:35:38 +0900

    x2

--- src/main/java/io/github/kahenteikou/kotlinoder/projfile/CoreJsonClass.kt
+++ src/main/java/io/github/kahenteikou/kotlinoder/projfile/CoreJsonClass.kt
@@ -7,6 +7,8 @@ class CoreJsonClass {
     var name: String=""
     @JsonProperty("transfers")
     var TransferList: List<CoreJson_TransferListElement> = emptyList()
-    
+    @JsonProperty("treeelements")
+    var TreeElements:List<CoreJsonSubTreeElement> = emptyList()
+
 
 }
--- src/main/java/io/github/kahenteikou/kotlinoder/projfile/CoreJsonSubTreeElement.kt
+++ src/main/java/io/github/kahenteikou/kotlinoder/projfile/CoreJsonSubTreeElement.kt
@@ -5,5 +5,5 @@ import java.util.*
 class CoreJsonSubTreeElement {
     var uuid: UUID=UUID.fromString("00000000-0000-0000-0000-000000000000")
     var targets:MutableMap<String,CoreJsonSubTreeConnectorElement> =mutableMapOf()
-    
+
 }
