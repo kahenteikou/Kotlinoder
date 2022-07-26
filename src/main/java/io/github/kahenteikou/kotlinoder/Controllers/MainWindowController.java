f8d4dcd09cc1545998e89da62df11891a702c3d9
kahenteikou <kokkiemouse@gmail.com>
2022-07-26 20:18:50 +0900

    2

--- src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.java
+++ src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.java
@@ -8,6 +8,6 @@ import java.util.ResourceBundle;
 public class MainWindowController  implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+
     }
 }
