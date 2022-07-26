6d47a38387802e08c5e33c63d59e2e1842d8ec60
kahenteikou <kokkiemouse@gmail.com>
2022-07-27 00:14:39 +0900

    x

--- src/main/java/io/github/kahenteikou/kotlinoder/App/MainApp.java
+++ src/main/java/io/github/kahenteikou/kotlinoder/App/MainApp.java
@@ -1,5 +1,6 @@
 package io.github.kahenteikou.kotlinoder.App;
 
+import io.github.kahenteikou.kotlinoder.assets.AssetStub;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +11,7 @@ import javafx.stage.Stage;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root= FXMLLoader.load(getClass().getResource("../assets/MainWindow.fxml"));
+        Parent root= FXMLLoader.load(AssetStub.class.getResource("MainWindow.fxml"));
         Scene scene=new Scene(root);
         stage.setTitle("MainWindow");
         stage.setScene(scene);
---
+++ src/main/java/io/github/kahenteikou/kotlinoder/assets/AssetStub.kt
@@ -0,0 +1,4 @@
+package io.github.kahenteikou.kotlinoder.assets
+
+class AssetStub {
+}
