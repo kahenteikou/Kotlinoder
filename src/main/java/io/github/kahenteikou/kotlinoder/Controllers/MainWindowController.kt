ef3e8117c1786ad3def809a05de5582042f93432
kahenteikou <kokkiemouse@gmail.com>
2022-07-27 23:07:32 +0900

    kotlin

--- src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.kt
+++ src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.kt
@@ -2,9 +2,13 @@ package io.github.kahenteikou.kotlinoder.Controllers
 
 import javafx.application.Platform
 import javafx.event.ActionEvent
+import javafx.fxml.FXML
 import javafx.fxml.Initializable
+import javafx.scene.layout.Pane
 
 class MainWindowController : Initializable {
+    @FXML
+    lateinit var view : Pane
     override fun initialize(location: java.net.URL?, resources: java.util.ResourceBundle?) {
 
     }
