7cd1794b7232b99a4ad03a7f2f4b49a320e7422b
kahenteikou <kokkiemouse@gmail.com>
2022-07-27 23:08:23 +0900

    late

--- src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.kt
+++ src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.kt
@@ -1,5 +1,6 @@
 package io.github.kahenteikou.kotlinoder.Controllers
 
+import eu.mihosoft.vrl.workflow.VFlow
 import javafx.application.Platform
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
@@ -9,6 +10,8 @@ import javafx.scene.layout.Pane
 class MainWindowController : Initializable {
     @FXML
     lateinit var view : Pane
+    lateinit private var rootPane: Pane
+    lateinit private var flow: VFlow
     override fun initialize(location: java.net.URL?, resources: java.util.ResourceBundle?) {
 
     }
