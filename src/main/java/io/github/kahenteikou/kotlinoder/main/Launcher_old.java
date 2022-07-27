b7a97a37af2a30814606273f21f6369c80d63e0d
kahenteikou <kokkiemouse@gmail.com>
2022-07-27 23:38:11 +0900

    r

--- src/main/java/io/github/kahenteikou/kotlinoder/main/Launcher_old.java
+++ src/main/java/io/github/kahenteikou/kotlinoder/main/Launcher_old.java
@@ -3,7 +3,7 @@ package io.github.kahenteikou.kotlinoder.main;
 import io.github.kahenteikou.kotlinoder.App.MainApp;
 import javafx.application.Application;
 
-public class Launcher {
+public class Launcher_old {
     public static void main(String[] args) {
         Application.launch(MainApp.class, args);
     }
