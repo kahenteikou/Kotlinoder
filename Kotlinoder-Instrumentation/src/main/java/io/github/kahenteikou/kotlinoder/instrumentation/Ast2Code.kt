61bd22e8e14c3fa68ccfc599540e4cc5c77639b1
kahenteikou <kokkiemouse@gmail.com>
2022-07-28 07:26:27 +0900

    x2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
@@ -1,6 +1,8 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 import org.jetbrains.kotlin.analyzer.AnalysisResult
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.context.ProjectContext
 
 class Ast2Code {
     companion object{
@@ -9,7 +11,7 @@ class Ast2Code {
 
         }
     }
-    fun exampleCodeGen(): AnalysisResult {
-
+    fun exampleCodeGen(): AnalysisResult? {
+        return null
     }
 }
