399c826a6e635f44db429296178386a862988c28
kahenteikou <kokkiemouse@gmail.com>
2022-07-27 23:41:33 +0900

    x2

--- src/main/java/io/github/kahenteikou/kotlinoder/main/Launcher.kt
+++ src/main/java/io/github/kahenteikou/kotlinoder/main/Launcher.kt
@@ -1,4 +1,13 @@
 package io.github.kahenteikou.kotlinoder.main
 
+import io.github.kahenteikou.kotlinoder.App.MainApp
+import javafx.application.Application
+
 class Launcher {
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(MainApp::class.java, *args)
+        }
+    }
 }
