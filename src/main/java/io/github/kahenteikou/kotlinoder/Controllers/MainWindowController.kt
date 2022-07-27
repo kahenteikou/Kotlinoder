57d01d72ca5e95d439bd91fe6e834914038fb34b
kahenteikou <kokkiemouse@gmail.com>
2022-07-27 23:20:46 +0900

    x2

--- src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.kt
+++ src/main/java/io/github/kahenteikou/kotlinoder/Controllers/MainWindowController.kt
@@ -1,5 +1,6 @@
 package io.github.kahenteikou.kotlinoder.Controllers
 
+import eu.mihosoft.vrl.workflow.FlowFactory
 import eu.mihosoft.vrl.workflow.VFlow
 import eu.mihosoft.vrl.workflow.fx.ScalableContentPane
 import javafx.application.Platform
@@ -16,7 +17,14 @@ class MainWindowController : Initializable {
     override fun initialize(location: java.net.URL?, resources: java.util.ResourceBundle?) {
         println("init")
         var canvas:ScalableContentPane=ScalableContentPane()
-        
+        canvas.maxScaleX=1.0
+        canvas.maxScaleY=1.0
+        view.children.add(canvas)
+        var root : Pane = Pane()
+        canvas.content=root
+        rootPane=root
+        flow=FlowFactory.newFlow()
+
     }
     fun File_Open_OnAction(event : ActionEvent){
 
