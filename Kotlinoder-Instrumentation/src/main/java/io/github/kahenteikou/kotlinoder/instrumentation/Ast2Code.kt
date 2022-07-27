0fe493fec4238a01325fe0eacabdacc99fe545eb
kahenteikou <kokkiemouse@gmail.com>
2022-07-28 07:15:27 +0900

    s2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
@@ -1,4 +1,10 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 class Ast2Code {
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+        }
+    }
 }
