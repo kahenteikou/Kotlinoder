905d73393cc68deea860cee12fbffce54d61ce77
kahenteikou <kokkiemouse@gmail.com>
2022-07-28 23:23:55 +0900

    x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
@@ -2,11 +2,19 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 
 import com.intellij.openapi.util.Disposer
 import org.jetbrains.kotlin.analyzer.AnalysisResult
+import org.jetbrains.kotlin.builtins.KotlinBuiltIns
+import org.jetbrains.kotlin.cli.common.messages.AnalyzerWithCompilerReport
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.languageVersionSettings
+import org.jetbrains.kotlin.context.ContextForNewModule
 import org.jetbrains.kotlin.context.ProjectContext
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.storage.StorageManager
 
+class CustomBuiltins(sm : StorageManager) : KotlinBuiltIns(sm)
 class Ast2Code {
     companion object{
         @JvmStatic
@@ -14,6 +22,7 @@ class Ast2Code {
 
         }
     }
+
     fun exampleCodeGen(): AnalysisResult? {
         val config= CompilerConfiguration()
         var dispos= Disposer.newDisposable()
@@ -22,6 +31,11 @@ class Ast2Code {
             config,
             EnvironmentConfigFiles.JVM_CONFIG_FILES
         )
+        val projcontext=ProjectContext(env.project,"Proj An")
+        val builtins= CustomBuiltins(projcontext.storageManager)
+        val context=ContextForNewModule(projcontext,
+        Name.special("<main>"),
+        builtins,null)
         
         return null
     }
