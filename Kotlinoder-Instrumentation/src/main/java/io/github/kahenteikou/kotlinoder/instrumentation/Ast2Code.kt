49de5ec108422679efb94405d8e2f97475a14c9f
kahenteikou <kokkiemouse@gmail.com>
2022-07-28 23:09:26 +0900

    env

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
@@ -1,6 +1,8 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
+import com.intellij.openapi.util.Disposer
 import org.jetbrains.kotlin.analyzer.AnalysisResult
+import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.context.ProjectContext
@@ -14,6 +16,13 @@ class Ast2Code {
     }
     fun exampleCodeGen(): AnalysisResult? {
         val config= CompilerConfiguration()
+        var dispos= Disposer.newDisposable()
+        val env=KotlinCoreEnvironment.createForProduction(
+            dispos,
+            config,
+            EnvironmentConfigFiles.JVM_CONFIG_FILES
+        )
+        
         return null
     }
 }
