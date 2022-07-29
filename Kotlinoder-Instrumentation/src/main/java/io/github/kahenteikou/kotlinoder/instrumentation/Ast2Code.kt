a2abfcec0fe7c1204a850eeca800acc21c3f28e8
kahenteikou <kokkiemouse@gmail.com>
2022-07-29 16:29:29 +0900

    x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
@@ -2,6 +2,8 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 
 import com.intellij.openapi.util.Disposer
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
 import org.jetbrains.kotlin.analyzer.AnalysisResult
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.cli.common.messages.AnalyzerWithCompilerReport
@@ -14,6 +16,9 @@ import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.context.ContextForNewModule
 import org.jetbrains.kotlin.context.ProjectContext
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.storage.StorageManager
 
 class CustomBuiltins(sm : StorageManager) : KotlinBuiltIns(sm)
@@ -21,25 +26,30 @@ class Ast2Code {
     companion object{
         @JvmStatic
         fun main(args: Array<String>) {
-
+            var A2=Ast2Code()
+            A2.exampleCodeGen();
         }
     }
 
-    fun exampleCodeGen(): AnalysisResult? {
-        /*
+    fun exampleCodeGen(): KtFile? {
+
         val config= CompilerConfiguration()
         var dispos= Disposer.newDisposable()
         val env=KotlinCoreEnvironment.createForProduction(
             dispos,
             config,
             EnvironmentConfigFiles.JVM_CONFIG_FILES
-        )
+        )/*
         val projcontext=ProjectContext(env.project,"Proj An")
         val builtins= CustomBuiltins(projcontext.storageManager)
         val context=ContextForNewModule(projcontext,
         Name.special("<main>"),
         builtins,null)*/
-        
-        return null
+        var psif= KtPsiFactory(env.project).createFile("""
+            fun main(args: Array<String>) {
+                println("Hello, world!")
+            }
+        """.trimIndent()) as KtFile
+        return psif
     }
 }
