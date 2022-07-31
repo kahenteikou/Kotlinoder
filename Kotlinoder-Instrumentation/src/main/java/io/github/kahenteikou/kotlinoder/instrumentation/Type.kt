773a61aa80b07f974c7807b879d0dba408130736
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 08:12:37 +0900

    ax

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Type.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Type.kt
@@ -2,7 +2,7 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 
 final class Type : IType {
     private final var packageName:String
-    
+    private final var shortName:String
     private fun validate(){
 
     }
