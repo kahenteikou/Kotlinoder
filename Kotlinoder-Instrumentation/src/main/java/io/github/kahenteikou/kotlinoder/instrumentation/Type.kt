7a80fa95d6f0b4fadfb959192fb4dbb8d0c03e07
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 08:15:06 +0900

    xxx

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Type.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Type.kt
@@ -4,6 +4,12 @@ final class Type : IType {
     private final var packageName:String
     private final var shortName:String
     private final var isReturnOrParamType:Boolean
+    constructor(packageName:String,shortName:String,isReturnOrParamType:Boolean) {
+        this.packageName = packageName
+        this.shortName = shortName
+        this.isReturnOrParamType = isReturnOrParamType
+        validate()
+    }
     private fun validate(){
 
     }
