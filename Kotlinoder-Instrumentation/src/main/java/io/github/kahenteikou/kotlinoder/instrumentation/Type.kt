43bb48fb0c079da48fdc68e5940e188ec62f752a
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 08:13:12 +0900

    2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Type.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Type.kt
@@ -3,6 +3,7 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 final class Type : IType {
     private final var packageName:String
     private final var shortName:String
+    private final var isReturnOrParamType:Boolean
     private fun validate(){
 
     }
