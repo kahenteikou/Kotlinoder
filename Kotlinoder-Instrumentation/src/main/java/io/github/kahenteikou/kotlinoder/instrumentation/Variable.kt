8b5c59fc20d831847459a1c60e9b0cd3f9e26192
kahenteikou <kokkiemouse@gmail.com>
2022-07-31 19:53:36 +0900

    2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Variable.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Variable.kt
@@ -1,5 +1,11 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 interface Variable {
+    val Name: String
+    val Type: String
+    val Value: Object
+    val isStatic:Boolean
+    val isConstant:Boolean
     
+
 }
