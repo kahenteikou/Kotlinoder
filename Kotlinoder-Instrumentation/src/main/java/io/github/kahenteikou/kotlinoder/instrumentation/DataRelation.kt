fb7a462fd28fce59e4c2277d9e9968fdc1b208cf
kahenteikou <kokkiemouse@gmail.com>
2022-07-31 20:01:54 +0900

    z2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/DataRelation.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/DataRelation.kt
@@ -1,14 +1,23 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 interface DataRelation {
+
     var Sender:Invocation
     var Receiver:Invocation
 }
 class DataRelationImpl:DataRelation{
-    private lateinit var _sender : Invocation
-    private lateinit var _receiver : Invocation
-    constructor(sender: Invocation, receiver: Invocation){
-        _sender = sender
-        _receiver = receiver
+    private lateinit var _Sender:Invocation
+    private lateinit var _Receiver:Invocation
+    constructor(sender:Invocation,receiver:Invocation){
+        _Sender=sender
+        _Receiver=receiver
     }
+
+    override var Sender: Invocation
+        get() = _Sender
+        set(value) {_Sender=value}
+    override var Receiver: Invocation
+        get() = _Receiver
+        set(value) {_Receiver=value}
+
 }
