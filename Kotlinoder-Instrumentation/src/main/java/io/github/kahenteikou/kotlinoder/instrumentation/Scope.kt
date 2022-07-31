d28bde0b26b9e5a0b990c4a14780c16473eb9ff9
kahenteikou <kokkiemouse@gmail.com>
2022-07-31 17:13:35 +0900

    z

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Scope.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Scope.kt
@@ -2,5 +2,8 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 
 interface Scope : CodeEntity{
     val Parent: Scope
-
+    val Type: ScopeType
+    val Name: String
+    val ScopeArgs: Array<Object>
+    
 }
