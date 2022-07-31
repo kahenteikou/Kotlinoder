6a54b99922f64918e29ac7bf42ac0676419a5934
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 08:47:52 +0900

    3x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Variable.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Variable.kt
@@ -1,5 +1,7 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
+
 interface Variable {
     fun getName(): String
     fun getType(): String
@@ -13,5 +15,11 @@ interface Variable {
 
 }
 class VariableImpl : Variable{
+    private final var scope:Scope
+    private final var type:IType
+    private final var varName:String
+    private var value:JvmType.Object
+    private var constant:Boolean
+    private var staticVar:Boolean
     
 }
