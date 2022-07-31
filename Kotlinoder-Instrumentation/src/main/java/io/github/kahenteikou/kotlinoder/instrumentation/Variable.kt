86033ca96cb288dacb21197cebedc47fcd1af530
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 08:54:18 +0900

    3e

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Variable.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Variable.kt
@@ -4,12 +4,12 @@ import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 interface Variable {
     fun getName(): String
-    fun getType(): String
-    fun getValue(): Object
-    fun setValue(o:Object)
+    fun getType(): IType
+    fun getValue(): Object?
+    fun setValue(o:Object?)
     fun isStatic():Boolean
-    fun isConstant():Boolean
-    fun setConstant(b:Boolean)
+    fun isConstant():Boolean?
+    fun setConstant(b:Boolean?)
     fun getScope():Scope
 
 
@@ -18,10 +18,10 @@ class VariableImpl : Variable{
     private final var scope:Scope
     private final var type:IType
     private final var varName:String
-    private var value:JvmType.Object?=null
+    private var value:Object?=null
     private var constant:Boolean?=null
     private var staticVar:Boolean
-    constructor(scope:Scope,type:IType,varName:String,value:JvmType.Object,constant:Boolean,staticVar:Boolean){
+    constructor(scope:Scope,type:IType,varName:String,value:Object,constant:Boolean,staticVar:Boolean){
         this.scope=scope
         this.type=type
         this.varName=varName
@@ -41,5 +41,37 @@ class VariableImpl : Variable{
         fun createStaticVar(scope: Scope, type: IType): VariableImpl {
             return VariableImpl(scope, type)
         }
+    }
+
+    override fun getName(): String {
+        return varName
+    }
+
+    override fun getType(): IType {
+        return type
+    }
+
+    override fun getValue(): Object? {
+        return value
+    }
+
+    override fun setValue(o: Object) {
+        TODO("Not yet implemented")
+    }
+
+    override fun isStatic(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isConstant(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun setConstant(b: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getScope(): Scope {
+        TODO("Not yet implemented")
     }
 }
