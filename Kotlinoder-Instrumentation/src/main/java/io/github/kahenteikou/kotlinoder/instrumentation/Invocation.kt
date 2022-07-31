45a9b3cf7cdc65638587bea588795017bb0961b5
kahenteikou <kokkiemouse@gmail.com>
2022-07-31 20:21:55 +0900

    23

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Invocation.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Invocation.kt
@@ -16,10 +16,14 @@ class InvocationImpl :Invocation{
     private final var MethodName:String
     private final var returnValueName:String
     private final var arguments:List<Variable> = ArrayList()
-    private final var constructor:Boolean
+    private final var Constructor:Boolean
     private final var Void: Boolean
     private var code:String
     private final var parent:Scope
     private var Static:Boolean
-    
+    constructor(parent:Scope,id:String,varName:String,methodName:String,
+                Constructor:Boolean,isVoid:Boolean,isStatic:Boolean,
+    retValName:String,vararg args:Variable){
+        
+    }
 }
