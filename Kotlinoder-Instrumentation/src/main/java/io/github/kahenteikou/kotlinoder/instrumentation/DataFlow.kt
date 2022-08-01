93376d57da7bdd467d8d6bef5135dee488d75f0a
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 10:08:17 +0900

    xxxx

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/DataFlow.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/DataFlow.kt
@@ -14,7 +14,13 @@ class DataFlowImpl :DataFlow{
     val _relations:ArrayList<DataRelation> = ArrayList()
     val _relationsForSender:ListMultimap<Invocation,DataRelation> = ArrayListMultimap.create()
     val _relationsForReceiver:ListMultimap<Invocation,DataRelation> = ArrayListMultimap.create()
-    
+    constructor(sender:Invocation,receiver:Invocation){
+        val relation:DataRelationImpl= DataRelationImpl(sender,receiver)
+        _relations.add(relation)
+        _relationsForSender.put(sender,relation)
+        println("sender:"+_relationsForSender.get(sender).size)
+        _relationsForReceiver.put(receiver,relation)
+    }
 
     override fun getRelations(): ArrayList<DataRelation> {
         return _relations
