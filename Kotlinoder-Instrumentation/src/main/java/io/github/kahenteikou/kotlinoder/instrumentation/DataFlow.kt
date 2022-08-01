0b49a96a99337492a2a2f7bfc484bd81d1d53da2
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 10:03:41 +0900

    3x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/DataFlow.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/DataFlow.kt
@@ -1,4 +1,9 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 interface DataFlow {
+    fun getRelations():ArrayList<DataRelation>
+    fun getRelationsForSender(invocation: Invocation):ArrayList<DataRelation>
+    fun getRelationsForReceiver(invocation: Invocation):ArrayList<DataRelation>
+    fun create(controlFlow: ControlFlow)
+    
 }
