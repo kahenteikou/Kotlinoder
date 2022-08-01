8a2a58b5b14ac2f89a0893b6e963ef97cd0e0d8a
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 10:05:15 +0900

    xxxx

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/DataFlow.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/DataFlow.kt
@@ -5,5 +5,25 @@ interface DataFlow {
     fun getRelationsForSender(invocation: Invocation):ArrayList<DataRelation>
     fun getRelationsForReceiver(invocation: Invocation):ArrayList<DataRelation>
     fun create(controlFlow: ControlFlow)
-    
+
+}
+class DataFlowImpl :DataFlow{
+    val _relations:ArrayList<DataRelation> = ArrayList()
+
+    override fun getRelations(): ArrayList<DataRelation> {
+        return _relations
+    }
+
+    override fun getRelationsForSender(invocation: Invocation): ArrayList<DataRelation> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRelationsForReceiver(invocation: Invocation): ArrayList<DataRelation> {
+        TODO("Not yet implemented")
+    }
+
+    override fun create(controlFlow: ControlFlow) {
+        TODO("Not yet implemented")
+    }
+
 }
