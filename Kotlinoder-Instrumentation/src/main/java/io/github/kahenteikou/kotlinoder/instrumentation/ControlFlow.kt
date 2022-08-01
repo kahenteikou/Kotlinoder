c2e97634c947a733ce948adb7301838bbf4fa4a7
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 09:15:23 +0900

    ex

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ControlFlow.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ControlFlow.kt
@@ -5,17 +5,49 @@ interface ControlFlow {
     fun callMethod(id: String,varName:String,mName:String,isVoid:Boolean,retValueName:String,vararg args:Variable):Invocation
     fun callStaticMethod(id:String,type:IType,mName:String,isVoid:Boolean,retValueName:String,vararg args:Variable):Invocation
     fun callScope(scope:Scope):ScopeInvocation
-    fun getInvocations():List<Invocation>
+    fun getInvocations():ArrayList<Invocation>
 }
 class ControlFlowImpl:ControlFlow{
-    private final val invocations:List<Invocation> =ArrayList()
-    override fun getInvocations(): List<Invocation> {
+    private final val invocations:ArrayList<Invocation> =ArrayList()
+    override fun createInstance(id: String, type: IType, varName: String, vararg args: Variable): Invocation {
+        val result:Invocation = InvocationImpl(parent,id,type.getFullClassName(),"<init>",true,false,true,varName,*args)
+        getInvocations().add(result)
+        return result
+    }
+
+    override fun callMethod(
+        id: String,
+        varName: String,
+        mName: String,
+        isVoid: Boolean,
+        retValueName: String,
+        vararg args: Variable
+    ): Invocation {
+        TODO("Not yet implemented")
+    }
+
+    override fun callStaticMethod(
+        id: String,
+        type: IType,
+        mName: String,
+        isVoid: Boolean,
+        retValueName: String,
+        vararg args: Variable
+    ): Invocation {
+        TODO("Not yet implemented")
+    }
+
+    override fun callScope(scope: Scope): ScopeInvocation {
+        TODO("Not yet implemented")
+    }
+
+    override fun getInvocations(): ArrayList<Invocation> {
         return invocations
     }
     private final var parent:Scope
     constructor(parent:Scope){
         this.parent = parent
     }
-    
+
 
 }
