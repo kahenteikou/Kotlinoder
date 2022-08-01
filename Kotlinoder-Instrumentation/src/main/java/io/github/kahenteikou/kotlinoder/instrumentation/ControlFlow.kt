cbcc822b949e6fd36952c2ad6ab0b6402da648ac
kahenteikou <kokkiemouse@gmail.com>
2022-08-01 09:08:03 +0900

    x2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ControlFlow.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ControlFlow.kt
@@ -3,5 +3,7 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 interface ControlFlow {
     fun createInstance(id:String,type: IType,varName:String,vararg args:Variable):Invocation
     fun callMethod(id: String,varName:String,mName:String,isVoid:Boolean,retValueName:String,vararg args:Variable):Invocation
+    fun callStaticMethod(id:String,type:IType,mName:String,isVoid:Boolean,retValueName:String,vararg args:Variable):Invocation
+    fun callScope(scope:Scope):ScopeInvocation
     
 }
