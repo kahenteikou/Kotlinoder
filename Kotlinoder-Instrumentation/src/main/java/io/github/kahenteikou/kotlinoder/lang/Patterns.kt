901d01f3b1db54cae5da091595c4221768e8e3c9
kahenteikou <kokkiemouse@gmail.com>
2022-08-03 05:48:54 +0900

    3x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Patterns.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Patterns.kt
@@ -1,9 +1,16 @@
 package io.github.kahenteikou.kotlinoder.lang
 
+import java.util.regex.Pattern
+
 class Patterns {
     companion object{
         @JvmStatic
-        final var IDENTIFIER_STRING="[a-zA-Z\\\\p{L}\$_][a-zA-Z\\\\p{L}\$_0-9]*"
-        final var PACKAGE_NAME_STRING="($IDENTIFIER_STRING)(\\.$IDENTIFIER_STRING)*"
+        final val IDENTIFIER_STRING="[a-zA-Z\\\\p{L}\$_][a-zA-Z\\\\p{L}\$_0-9]*"
+        @JvmStatic
+        final val PACKAGE_NAME_STRING="($IDENTIFIER_STRING)(\\.$IDENTIFIER_STRING)*"
+        @JvmStatic
+        final val PACKAGE_NAME= Pattern.compile(PACKAGE_NAME_STRING,
+        Pattern.DOTALL)
+
     }
 }
