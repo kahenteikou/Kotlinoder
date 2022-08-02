f49a378decd7680fc621d0d2b4882a19cfd911d2
kahenteikou <kokkiemouse@gmail.com>
2022-08-03 05:47:23 +0900

    zx

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Patterns.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Patterns.kt
@@ -2,6 +2,8 @@ package io.github.kahenteikou.kotlinoder.lang
 
 class Patterns {
     companion object{
+        @JvmStatic
         final var IDENTIFIER_STRING="[a-zA-Z\\\\p{L}\$_][a-zA-Z\\\\p{L}\$_0-9]*"
+        final
     }
 }
