d9cd1a5f2afad861293ade3cbdcde4358862d16f
kahenteikou <kokkiemouse@gmail.com>
2022-08-03 05:32:13 +0900

    386

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Keywords.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Keywords.kt
@@ -2,5 +2,14 @@ package io.github.kahenteikou.kotlinoder.lang
 
 class Keywords {
     private var keywords:Set<String> = HashSet<String>()
-    
+    companion object{
+        init{
+
+        }
+        @JvmStatic
+        fun readKeyWords(codeName:String):String{
+            var code:String=""
+            return code
+        }
+    }
 }
