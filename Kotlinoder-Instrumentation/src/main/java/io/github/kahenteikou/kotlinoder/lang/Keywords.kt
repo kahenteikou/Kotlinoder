bead74258e5e6d7238a0aaf3e2669be7b77faf3d
kahenteikou <kokkiemouse@gmail.com>
2022-08-03 05:28:00 +0900

    2plus

---
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Keywords.kt
@@ -0,0 +1,5 @@
+package io.github.kahenteikou.kotlinoder.lang
+
+class Keywords {
+    private var keywords:Set<String> = HashSet<String>()
+}
--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/VLangUtils.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/VLangUtils.kt
@@ -3,7 +3,18 @@ package io.github.kahenteikou.kotlinoder.lang
 class VLangUtils {
     companion object{
         @JvmStatic
-        fun isPackageNameValid(packageName: String): Boolean {
+        fun isPackageNameValid(packageName: String?): Boolean {
+            val packageName2=packageName
+            if("".equals(packageName2)){
+                return true
+            }
+            if(packageName2 == null){
+                packageName2=""
+            }
+            val identifiers=packageName2.split("\\.")
+            for(id in identifiers){
+                if(KeyWords)
+            }
         }
     }
 }
