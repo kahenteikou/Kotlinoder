783ddb29b2ffb78f30009ba87611b6be26294c20
kahenteikou <kokkiemouse@gmail.com>
2022-08-03 05:47:59 +0900

    r

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Patterns.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Patterns.kt
@@ -4,6 +4,6 @@ class Patterns {
     companion object{
         @JvmStatic
         final var IDENTIFIER_STRING="[a-zA-Z\\\\p{L}\$_][a-zA-Z\\\\p{L}\$_0-9]*"
-        final
+        final var PACKAGE_NAME_STRING="($IDENTIFIER_STRING)(\\.$IDENTIFIER_STRING)*"
     }
 }
