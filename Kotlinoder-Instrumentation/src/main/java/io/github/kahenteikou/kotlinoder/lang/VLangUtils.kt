1ec686973fb327d3420d4f741583b7691adf2a33
kahenteikou <kokkiemouse@gmail.com>
2022-08-03 05:24:26 +0900

    av2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/VLangUtils.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/VLangUtils.kt
@@ -1,4 +1,9 @@
 package io.github.kahenteikou.kotlinoder.lang
 
 class VLangUtils {
+    companion object{
+        @JvmStatic
+        fun isPackageNameValid(packageName: String): Boolean {
+        }
+    }
 }
