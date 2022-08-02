40884c31612ba4489f6868ef53253c3a4d6f6df8
kahenteikou <kokkiemouse@gmail.com>
2022-08-03 05:33:04 +0900

    2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Keywords.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/lang/Keywords.kt
@@ -1,5 +1,9 @@
 package io.github.kahenteikou.kotlinoder.lang
 
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+
 class Keywords {
     private var keywords:Set<String> = HashSet<String>()
     companion object{
@@ -8,7 +12,12 @@ class Keywords {
         }
         @JvmStatic
         fun readKeyWords(codeName:String):String{
+            val iStream:InputStream=Keywords::class.java.getResourceAsStream(codeName)
+            val reader=BufferedReader(InputStreamReader(iStream))
             var code:String=""
+            try{
+                
+            }
             return code
         }
     }
