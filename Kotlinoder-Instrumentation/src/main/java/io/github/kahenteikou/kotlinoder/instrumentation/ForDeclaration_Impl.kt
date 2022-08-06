58d0815ee40964dc81c00aeb9491012b72dd0abd
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 22:37:57 +0900

    x2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ForDeclaration_Impl.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ForDeclaration_Impl.kt
@@ -2,3 +2,15 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 
 class ForDeclaration_Impl {
 }
+class ForDeclarationMetaData {
+    private var varName:String
+    private var _from:Int
+    private var _to:Int
+    private var _inc:Int
+    constructor(varName:String,_from:Int,_to:Int,_inc:Int){
+        this.varName=varName
+        this._from=_from
+        this._to=_to
+        this._inc=_inc
+    }
+}
