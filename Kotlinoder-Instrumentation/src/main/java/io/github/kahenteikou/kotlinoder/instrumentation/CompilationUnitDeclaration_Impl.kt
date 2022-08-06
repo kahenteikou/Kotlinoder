df6f1832a0250790b28c46fdd7460ac02d56a168
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 19:08:57 +0900

    c

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CompilationUnitDeclaration_Impl.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CompilationUnitDeclaration_Impl.kt
@@ -1,6 +1,9 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
-class CompilationUnitDeclaration_Impl {
+class CompilationUnitDeclaration_Impl(id:String,parent:Scope?,name:String,packageName: String):
+ScopeImpl(id,parent,ScopeType.COMPILATION_UNIT,name,null),CompilationUnitDeclaration{
+    
+
 }
 final class CompilationUnitMetaData {
     private final var packageName:String
