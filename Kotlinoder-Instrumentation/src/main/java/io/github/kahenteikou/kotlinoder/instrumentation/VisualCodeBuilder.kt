621c2b2c89f71a4a39c5a9ca409a10994d15ff9f
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 22:47:37 +0900

    3f

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder.kt
@@ -5,5 +5,6 @@ interface VisualCodeBuilder {
     fun assignConstant(scope:Scope?,varName:String,constant:Any?)
     fun assignVariable(scope:Scope?,varNameDest:String,varNameSrc:String)
     fun createInstance(scope:Scope?,type:IType,varName:String,vararg args:Variable?)
+    fun createVariable(scope:Scope?,type:IType,varName:String):Variable
     
 }
