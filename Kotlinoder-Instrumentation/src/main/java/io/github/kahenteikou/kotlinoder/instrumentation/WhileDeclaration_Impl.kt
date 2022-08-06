b316280ef7fe75edc94ecd1618f9f3696cad205f
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 22:59:08 +0900

    3x

---
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/WhileDeclaration_Impl.kt
@@ -0,0 +1,11 @@
+package io.github.kahenteikou.kotlinoder.instrumentation
+
+class WhileDeclaration_Impl {
+}
+class WhileDeclarationMetaData {
+    private final var check:Invocation
+    constructor(check:Invocation){
+        this.check = check
+    }
+    
+}
