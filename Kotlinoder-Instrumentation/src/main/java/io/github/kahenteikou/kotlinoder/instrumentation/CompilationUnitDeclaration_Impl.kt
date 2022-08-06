b2502870e21df5c60e3fe6d6f0b35d7f11ea76f0
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 19:10:17 +0900

    3d

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CompilationUnitDeclaration_Impl.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CompilationUnitDeclaration_Impl.kt
@@ -1,8 +1,16 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
+import io.github.kahenteikou.kotlinoder.lang.VLangUtils
+
 class CompilationUnitDeclaration_Impl(id:String,parent:Scope?,name:String,packageName: String):
 ScopeImpl(id,parent,ScopeType.COMPILATION_UNIT,name,null),CompilationUnitDeclaration{
-    
+    private var metadata:CompilationUnitMetaData
+    init{
+        if(!VLangUtils.isPackageNameValid(packageName)){
+            throw IllegalArgumentException("Invalid package name: $packageName")
+        }
+        metadata=CompilationUnitMetaData(packageName)
+    }
 
 }
 final class CompilationUnitMetaData {
