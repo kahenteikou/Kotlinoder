9f80dd63eb096b7764105a26c1d16e8a88a5e243
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 16:41:01 +0900

    x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/IParameter.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/IParameter.kt
@@ -1,4 +1,13 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 interface IParameter {
+    /**
+     * name
+     */
+    fun getName():String
+
+    /**
+     * type
+     */
+    fun getType():IType
 }
