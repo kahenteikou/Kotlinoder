2c8164607fab20ead8875bcb730e558a8216bff0
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 23:13:04 +0900

    ed

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder_Impl.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder_Impl.kt
@@ -26,5 +26,14 @@ class VisualCodeBuilder_Impl : VisualCodeBuilder {
     override fun declareCompilationUnit(name: String, packageName: String): CompilationUnitDeclaration {
         return CompilationUnitDeclaration_Impl(idRequest.request(),null,name,packageName)
     }
-    
+    override fun createVariable(scope:Scope?,type:IType,varName:String):Variable{
+        var resultkun: Variable? =scope!!.createVariable(type,varName)
+        variables.push(varName)
+        return resultkun!!
+    }
+    fun createVariable(scope:Scope?,type:IType):Variable{
+        var resultkun=scope!!.createVariable(type)
+        variables.push(resultkun!!.getName())
+        return resultkun!!
+    }
 }
