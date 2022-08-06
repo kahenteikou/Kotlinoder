b2c9dc2ed085d5058ffcd651dde3e06eb0a41659
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 19:07:30 +0900

    ed

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CompilationUnitDeclaration_Impl.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CompilationUnitDeclaration_Impl.kt
@@ -2,3 +2,12 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 
 class CompilationUnitDeclaration_Impl {
 }
+final class CompilationUnitMetaData {
+    private final var packageName:String
+    constructor(packageName:String) {
+        this.packageName = packageName
+    }
+    fun getPackageName():String {
+        return packageName
+    }
+}
