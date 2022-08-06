ba8af3c94915a13381cfbc3d57ce44582844719f
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 23:08:13 +0900

    x2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder_Impl.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder_Impl.kt
@@ -1,8 +1,16 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
+import eu.mihosoft.vrl.workflow.FlowFactory
 import java.util.*
+import eu.mihosoft.vrl.workflow.IdGenerator
 
 class VisualCodeBuilder_Impl : VisualCodeBuilder {
     private final val variables: Stack<String> = Stack()
-
+    private var idRequest:IdRequest= object : IdRequest {
+        val generator: IdGenerator = FlowFactory.newIdGenerator()
+        override fun request(): String {
+            return generator.newId()
+        }
+    }
+    
 }
