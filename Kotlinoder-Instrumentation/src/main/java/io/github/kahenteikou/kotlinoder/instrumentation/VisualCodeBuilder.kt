99e34ec9bed049b59e935db5eb7d7d38b973b2e7
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 22:49:31 +0900

    dec

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder.kt
@@ -1,11 +1,11 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 interface VisualCodeBuilder {
-    fun declareCompilationUnit(name:String,packageName:String)
+    fun declareCompilationUnit(name:String,packageName:String):CompilationUnitDeclaration
     fun assignConstant(scope:Scope?,varName:String,constant:Any?)
     fun assignVariable(scope:Scope?,varNameDest:String,varNameSrc:String)
     fun createInstance(scope:Scope?,type:IType,varName:String,vararg args:Variable?)
     fun createVariable(scope:Scope?,type:IType,varName:String):Variable
     fun declareFor(scope:Scope?,varName:String,from:Int,to:Int,inc:Int):ForDeclaration
-    
+    fun declareClass(scope:CompilationUnitDeclaration,type:IType,modifiers:IModifiers,extends:IExtends,implements:IExtends):ClassDeclaration
 }
