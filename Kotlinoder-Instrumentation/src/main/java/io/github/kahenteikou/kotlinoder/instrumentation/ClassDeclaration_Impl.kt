8672ec098dece78a6645218d22abd237e787319f
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 17:32:41 +0900

    3s

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ClassDeclaration_Impl.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ClassDeclaration_Impl.kt
@@ -3,10 +3,15 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 class ClassDeclaration_Impl {
 }
 final class ClassDeclarationMetaData {
-    private final var type:Type
-    private final var modifiers:IModifiers
-    private final var extends : IExtends
-    private final var implements:IExtends
+    private final var type:IType
+    private final var modifiers:IModifiers?
+    private final var extends : IExtends?
+    private final var implements:IExtends?
     private final var declaredMethods:MutableList<MethodDeclaration> = ArrayList()
-    
+    constructor(type:IType,modifiers:IModifiers?,extends:IExtends?,implements:IExtends?) {
+        this.type = type
+        this.modifiers = modifiers
+        this.extends = extends
+        this.implements = implements
+    }
 }
