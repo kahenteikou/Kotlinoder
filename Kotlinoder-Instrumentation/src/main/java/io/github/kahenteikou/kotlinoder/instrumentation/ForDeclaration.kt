53510915c251154cc6441ebcf2e496f32cedc9de
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 22:35:48 +0900

    x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ForDeclaration.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ForDeclaration.kt
@@ -1,4 +1,8 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 interface ForDeclaration :Scope{
+    fun getVarName():String
+    fun getFrom():Int
+    fun getTo():Int
+    fun getInc():Int
 }
