4a0383ea8cb90b441ada8eca317698408a5fb138
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 23:04:27 +0900

    req

---
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/IdRequest.kt
@@ -0,0 +1,5 @@
+package io.github.kahenteikou.kotlinoder.instrumentation
+
+interface IdRequest {
+    fun request():String
+}
--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder_Impl.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VisualCodeBuilder_Impl.kt
@@ -1,5 +1,8 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
+import java.util.*
+
 class VisualCodeBuilder_Impl : VisualCodeBuilder {
-    
+    private final val variables: Stack<String> = Stack()
+
 }
