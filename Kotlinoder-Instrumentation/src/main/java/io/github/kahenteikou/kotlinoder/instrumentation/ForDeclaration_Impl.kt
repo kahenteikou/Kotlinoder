350546a387d513e9da543065d50bf60db6400761
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 22:43:38 +0900

    2s

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ForDeclaration_Impl.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ForDeclaration_Impl.kt
@@ -3,6 +3,32 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 class ForDeclaration_Impl(id:String,parent:Scope?,varName:String,from:Int,to:Int,inc:Int):
 ScopeImpl(id,parent,ScopeType.FOR,ScopeType.FOR.name,ForDeclarationMetaData(varName,from,to,inc)),
 ForDeclaration {
+    private final var metadata:ForDeclarationMetaData
+    init{
+        val forceIncrement=(from<to)
+        val equal=(from==to)
+        if(forceIncrement&&!equal&&inc<=0) {
+            throw IllegalArgumentException("For loop cannot have negative or zero increment!")
+        }else if(!forceIncrement&&!equal&&inc>=0) {
+            throw IllegalArgumentException("For loop cannot have positive or zero increment!")
+        }
+        
+    }
+    override fun getVarName(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getFrom(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTo(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getInc(): Int {
+        TODO("Not yet implemented")
+    }
 
 }
 class ForDeclarationMetaData {
