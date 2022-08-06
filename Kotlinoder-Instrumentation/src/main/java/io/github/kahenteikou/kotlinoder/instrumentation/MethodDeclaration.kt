fbe3219de652094cf7c99b285776670951e69f44
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 16:54:51 +0900

    2

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/MethodDeclaration.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/MethodDeclaration.kt
@@ -1,4 +1,8 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 interface MethodDeclaration {
+    fun getModifiers():IModifiers
+    fun getParameters():IParameters
+    fun getParameterAsVariable(p: IParameter) : Variable
+    fun getReturnType(): IType
 }
