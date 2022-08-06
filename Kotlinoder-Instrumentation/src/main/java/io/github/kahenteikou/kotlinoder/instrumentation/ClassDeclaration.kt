c3d42114a181bf41868944567766356ce2a741c0
kahenteikou <kokkiemouse@gmail.com>
2022-08-06 16:57:06 +0900

    x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ClassDeclaration.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/ClassDeclaration.kt
@@ -1,4 +1,10 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 interface ClassDeclaration : Scope {
+    fun getClassModifiers():IModifiers
+    fun getClassType():IType
+    fun getExtends():IExtends
+    fun getImplements():IExtends
+    fun getDeclaredMethods():MutableList<MethodDeclaration>
+    fun declareMethod(id:String,modifiers:IModifiers,returnType:IType,methodName:String,params:IParameters):MethodDeclaration
 }
