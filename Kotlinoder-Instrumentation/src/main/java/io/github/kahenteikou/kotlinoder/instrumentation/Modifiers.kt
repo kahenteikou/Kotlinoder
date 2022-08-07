c427ebcc1068f4e944615280172ce6bf44cce3d9
kahenteikou <kokkiemouse@gmail.com>
2022-08-07 21:36:22 +0900

    x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Modifiers.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Modifiers.kt
@@ -1,4 +1,20 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
-final class Modifiers {
+import java.util.*
+import kotlin.collections.ArrayList
+
+final class Modifiers :IModifiers{
+    private final var modifiers:MutableList<Modifier> = ArrayList()
+    private var readOnlyModifiers:List<Modifier>?=null
+    constructor(vararg modifiers:Modifier){
+        this.modifiers.addAll(modifiers)
+    }
+
+    override fun getModifiers(): MutableList<Modifier> {
+        if(readOnlyModifiers==null){
+            readOnlyModifiers= Collections.unmodifiableList(modifiers)
+        }
+        return readOnlyModifiers as MutableList<Modifier>
+    }
+
 }
