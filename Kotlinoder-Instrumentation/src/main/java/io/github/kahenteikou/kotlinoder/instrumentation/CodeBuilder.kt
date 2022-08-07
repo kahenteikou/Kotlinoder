208f3a48ede979ded9bb75d496a77dce8bac492d
kahenteikou <kokkiemouse@gmail.com>
2022-08-07 09:52:28 +0900

    3x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CodeBuilder.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CodeBuilder.kt
@@ -3,7 +3,12 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 class CodeBuilder {
     companion object{
         private class Line{
-
+            private var indentCount:Int
+            private var line:String
+            constructor(line:String,indentCount:Int){
+                this.indentCount = indentCount
+                this.line = line
+            }
         }
     }
 }
