7d674f5109847dde3d6c35e98660ce1990a4e1de
kahenteikou <kokkiemouse@gmail.com>
2022-08-07 09:57:25 +0900

    cxcc

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CodeBuilder.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CodeBuilder.kt
@@ -2,6 +2,10 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 
 class CodeBuilder {
     private var indentString:String="    "
+    private var currentLine:StringBuilder = StringBuilder()
+    private var cursorPos:Int=0
+    private var code:ArrayList<Line> =ArrayList()
+    
     companion object{
         private class Line{
             private var indentCount:Int
