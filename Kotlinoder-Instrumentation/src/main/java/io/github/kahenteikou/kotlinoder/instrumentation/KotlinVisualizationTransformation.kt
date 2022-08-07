dabcf13eeca8ca77d08b44dee3b90c7680c4370d
kahenteikou <kokkiemouse@gmail.com>
2022-08-08 07:55:35 +0900

    x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/KotlinVisualizationTransformation.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/KotlinVisualizationTransformation.kt
@@ -2,27 +2,36 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 
 import eu.mihosoft.vrl.workflow.FlowFactory
 import eu.mihosoft.vrl.workflow.IdGenerator
+import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtVisitor
 import java.util.*
 
 class KotlinVisualizationTransformation {
 }
-class KotlinCodeVisitor : KtVisitor<Unit, VisualCodeBuilder_Impl> {
+class KotlinCodeVisitor : KtVisitor<KtFile, VisualCodeBuilder_Impl> {
     private var codeBuilder:VisualCodeBuilder_Impl
+    private var ktFile:KtFile
     private var rootScope:Scope?=null
     private var currentScope:Scope?=null
     private var lastMethod:Invocation?=null
     private var vIdStack: Stack<String> = Stack()
     private var generator:IdGenerator=FlowFactory.newIdGenerator()
-    constructor(codeBuilder:VisualCodeBuilder_Impl){
+    constructor(ktFile:KtFile,codeBuilder:VisualCodeBuilder_Impl){
 
         this.codeBuilder=codeBuilder
+        this.ktFile=ktFile
         codeBuilder.setIdRequest(object : IdRequest{
             override fun request(): String {
                 return requestId()
             }
 
         })
+
+        this.rootScope=codeBuilder.declareCompilationUnit(
+            ktFile.name,
+            "undefined"
+        )
+        this.currentScope=rootScope
     }
     private fun requestId():String{
         var result:String=""
