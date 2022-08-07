b8003563a4066a03f6a9626339aa0aa6ecd416fa
kahenteikou <kokkiemouse@gmail.com>
2022-08-07 13:35:48 +0900

    3e

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Scope2Code.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Scope2Code.kt
@@ -3,5 +3,15 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 class Scope2Code {
 }
 final class Utils {
-    
+    companion object{
+        private var modifierNames:MutableMap<Modifier,String> = HashMap()
+        init{
+            modifierNames[Modifier.ABSTRACT] = "abstract"
+            modifierNames[Modifier.FINAL]="final"
+            modifierNames[Modifier.PRIVATE]="private"
+            modifierNames[Modifier.PROTECTED]="protected"
+            modifierNames[Modifier.PUBLIC]="public"
+            modifierNames[Modifier.STATIC]="@JvmStatic"
+        }
+    }
 }
