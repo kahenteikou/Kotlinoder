efbe3c56f50edde7b5d3f227d9d7f7e5962eb07e
kahenteikou <kokkiemouse@gmail.com>
2022-08-07 09:46:57 +0900

    comp

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CodeRenderer.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CodeRenderer.kt
@@ -1,5 +1,5 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
-
+//complete
 interface CodeRenderer <T : CodeEntity> {
     fun render(entity: T): String
     fun render(entity:T,cb:CodeBuilder)
