e17512ec106800939cdbc62e16abc2a8b874e73b
kahenteikou <kokkiemouse@gmail.com>
2022-08-08 11:15:21 +0900

    e2

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/MainWindowController.kt
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/MainWindowController.kt
@@ -1,11 +1,40 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
 import javafx.fxml.Initializable
+import javafx.scene.control.TextArea
+import javafx.scene.input.KeyEvent
+import javafx.scene.layout.Pane
 import java.net.URL
 import java.util.*
 
 class MainWindowController : Initializable {
+    @FXML
+    lateinit var editor: TextArea
+    @FXML
+    lateinit var view:Pane
     override fun initialize(location: URL?, resources: ResourceBundle?) {
+
+    }
+    @FXML
+    fun onKeyTyped(event: KeyEvent){
+
+    }
+    @FXML
+    fun onLoadAction(e:ActionEvent){
+
+    }
+    @FXML
+    fun onSaveAction(e:ActionEvent){
+
+    }
+    @FXML
+    fun onSaveAsAction(e:ActionEvent){
+
+    }
+    @FXML
+    fun onCloseAction(e:ActionEvent){
 
     }
 
