897dac9387a217bbbd2e266855988552e279c2cf
kahenteikou <kokkiemouse@gmail.com>
2022-08-08 11:19:48 +0900

    3d

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/MainWindowController.kt
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/MainWindowController.kt
@@ -1,5 +1,9 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main
 
+import eu.mihosoft.vrl.workflow.Connector
+import eu.mihosoft.vrl.workflow.VFlow
+import eu.mihosoft.vrl.workflow.VNode
+import io.github.kahenteikou.kotlinoder.instrumentation.CodeEntity
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
@@ -8,12 +12,18 @@ import javafx.scene.input.KeyEvent
 import javafx.scene.layout.Pane
 import java.net.URL
 import java.util.*
+import kotlin.collections.HashMap
 
 class MainWindowController : Initializable {
     @FXML
     lateinit var editor: TextArea
     @FXML
     lateinit var view:Pane
+    private var rootPane:Pane
+    private var flow:VFlow
+    private var invocationNodes:MutableMap<CodeEntity, VNode> = HashMap()
+    private var variableConnectors:MutableMap<String,Connector> = HashMap()
+    
     override fun initialize(location: URL?, resources: ResourceBundle?) {
 
     }
