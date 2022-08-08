1174215f64936e75fc37f83780061ad545c03acc
kahenteikou <kokkiemouse@gmail.com>
2022-08-08 10:40:45 +0900

    x

--- Kotlinoder-CodeVisualization/build.gradle.kts
+++ Kotlinoder-CodeVisualization/build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
     id("java")
     id("application")
+    id("org.jetbrains.kotlin.jvm") version ("1.7.10")
+    id("com.github.johnrengelman.shadow") version("7.1.2")
+    id("org.openjfx.javafxplugin") version ("0.0.13")
 }
