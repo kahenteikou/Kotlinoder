ca24e69b4705927a2986ebe1b236ae8547711202
kahenteikou <kokkiemouse@gmail.com>
2022-08-08 10:58:42 +0900

    x

---
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/MainWindowController.kt
@@ -0,0 +1,12 @@
+package io.github.kahenteikou.kotlinoder.codevisualization.main
+
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.*
+
+class MainWindowController : Initializable {
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        
+    }
+
+}
