6d3e7a7716f4d5633b0b5ee433c00667d832154f
kahenteikou <kokkiemouse@gmail.com>
2022-08-08 10:48:43 +0900

    x

--- Kotlinoder-CodeVisualization/build.gradle.kts
+++ Kotlinoder-CodeVisualization/build.gradle.kts
@@ -10,3 +10,11 @@ version="1.0-SNAPSHOT"
 application{
     mainClass.set("io.github.kahenteikou.kotlinoder.codevisualization.main.Launcher")
 }
+dependencies{
+    implementation("org.jfxtras:jfxtras-labs:9.0-r1")
+    implementation("eu.mihosoft.vrl.workflow:vworkflows-core:0.2.5.0")
+    implementation("eu.mihosoft.vrl.workflow:vworkflows-fx:0.2.5.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
+    implementation("org.jetbrains.kotlin:kotlin-test:1.7.10")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.7.10")
+}
