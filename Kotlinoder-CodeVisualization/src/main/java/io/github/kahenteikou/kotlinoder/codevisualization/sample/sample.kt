282fa1f06ddf692362e543bd76a5a1b36bfddc90
kahenteikou <kokkiemouse@gmail.com>
2022-08-10 09:06:59 +0900

    v

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/sample/sample.kt
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/sample/sample.kt
@@ -2,6 +2,8 @@ package io.github.kahenteikou.kotlinoder.codevisualization.sample
 
 public class sample {
     public fun sample22(v1:Int):Int{
+        println("xxxxx")
+
         return v1
     }
 }
--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Scope2Code.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Scope2Code.kt
@@ -68,6 +68,7 @@ class Scope2Code {
             var mx:MethodDeclaration=builder.declareMethod(myFileClass,
                 Modifiers(Modifier.PUBLIC),Type("void"),"mx",
                 Parameters(Parameter(Type("Int"),"v1")))
+            builder.invokeStaticMethod(mx,Type(""),"println",true,"",mx.getVariable("v1")!!)
 
             return myFile
         }
