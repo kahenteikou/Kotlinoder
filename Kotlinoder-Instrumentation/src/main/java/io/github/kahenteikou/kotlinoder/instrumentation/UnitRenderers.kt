623bccfd3d2eff29e44fa17f7ee4c2619375fb51
kahenteikou <kokkiemouse@gmail.com>
2022-08-12 18:49:19 +0900

    x

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/UnitRenderers.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/UnitRenderers.kt
@@ -1,5 +1,11 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
 class CompilationUnitRenderer:CodeRenderer<CompilationUnitDeclaration>{
+    private var classDeclarationRenderer:CodeRenderer<ClassDeclaration>?=null
+    constructor(){
 
+    }
+    constructor(cr:CodeRenderer<ClassDeclaration>){
+        this.classDeclarationRenderer=cr
+    }
 }
