0bcabd3b26310ac23f0e6a4083429f763d0d1a23
kahenteikou <kokkiemouse@gmail.com>
2022-08-12 18:50:43 +0900

    cx

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/UnitRenderers.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/UnitRenderers.kt
@@ -1,5 +1,8 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
+import ktast.ast.Node
+import ktast.ast.psi.Parser
+
 class CompilationUnitRenderer:CodeRenderer<CompilationUnitDeclaration>{
     private var classDeclarationRenderer:CodeRenderer<ClassDeclaration>?=null
     constructor(){
@@ -7,5 +10,32 @@ class CompilationUnitRenderer:CodeRenderer<CompilationUnitDeclaration>{
     }
     constructor(cr:CodeRenderer<ClassDeclaration>){
         this.classDeclarationRenderer=cr
+    }
+
+    fun getClassDeclarationRenderer():CodeRenderer<ClassDeclaration>{
+        return classDeclarationRenderer!!
+    }
+    fun setClassDeclarationRenderer(classDeclarationRenderer:CodeRenderer<ClassDeclaration>){
+        this.classDeclarationRenderer=classDeclarationRenderer
+    }
+
+    override fun render(entity: CompilationUnitDeclaration): Node {
+        var nd= Parser.parseFile("")
+        render(entity,nd)
+        return nd
+    }
+
+    override fun render(e: CompilationUnitDeclaration, nd: Node) {
+
+        if(e.getPackageName()!=null||e.getPackageName()!!.isEmpty()){
+            if(nd is Node.KotlinEntry){
+                //nd.packageDirective=Node.PackageDirective(e.getPackageName()!!)
+            }
+            //cb.append("package ").append(e.getPackageName()!!).newLine().newLine()
+
+        }
+        for(cd in e.getDeclaredClasses()){
+            //classDeclarationRenderer!!.render2(cd,nd)
+        }
     }
 }
