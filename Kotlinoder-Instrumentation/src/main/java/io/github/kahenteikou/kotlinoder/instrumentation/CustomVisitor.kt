0c1e3e34288b4aba59a5faec76b3a3c03ab5a6d4
kahenteikou <kokkiemouse@gmail.com>
2022-08-12 21:40:54 +0900

    ed

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CustomVisitor.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CustomVisitor.kt
@@ -1,5 +1,14 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
+import ktast.ast.Node
+
 open class CustomVisitor {
-    
+    companion object{
+        fun visit(v:Node,fn: (v: Node, parent: Node?) -> Unit)=object:CustomVisitor(){
+            override fun visit(v: Node, parent: Node?) {
+                fn(v,parent)
+                super.visit(v,parent)
+            }
+        }
+    }
 }
