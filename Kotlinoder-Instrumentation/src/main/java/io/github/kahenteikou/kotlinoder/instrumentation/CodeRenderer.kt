35275242a2843683692028bbeece8d064ef55636
kahenteikou <kokkiemouse@gmail.com>
2022-08-12 18:46:38 +0900

    cx

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CodeRenderer.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/CodeRenderer.kt
@@ -1,4 +1,8 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
+import ktast.ast.Node
+
 interface CodeRenderer <T : CodeEntity> {
+    fun render(entity: T): Node
+    fun render(entity:T,nd:Node)
 }
