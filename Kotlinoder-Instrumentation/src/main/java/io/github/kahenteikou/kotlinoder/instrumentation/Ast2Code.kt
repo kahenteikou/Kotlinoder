2ce05f37e28b27fe0b2c9a47c1c6716ed0fd0c49
kahenteikou <kokkiemouse@gmail.com>
2022-08-12 18:04:08 +0900

    xxxx

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Ast2Code.kt
@@ -11,7 +11,7 @@ class Ast2Code {
     }
 
     fun exampleCodeGen() {
-        
+
 
     }
 }
--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Scope2Code.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/Scope2Code.kt
@@ -1,19 +1,6 @@
 package io.github.kahenteikou.kotlinoder.instrumentation
 
-import com.intellij.openapi.util.Disposer
-import com.intellij.psi.PsiManager
-import com.intellij.testFramework.LightVirtualFile
 import io.github.kahenteikou.kotlinoder.lang.VLangUtils
-import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
-import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
-import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.diagnostics.rendering.renderParameter
-import org.jetbrains.kotlin.idea.KotlinFileType
-import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.psi.KtPsiFactory
-import org.jetbrains.kotlin.psi.classVisitor
-import org.jetbrains.kotlin.psi.psiUtil.containingClass
 
 class Scope2Code {
     companion object{
@@ -85,7 +72,7 @@ class Scope2Code {
             println(renderer.render(scope))
             println("demo")
             //kotlin parser
-
+            /*
             val config= CompilerConfiguration()
             var dispos= Disposer.newDisposable()
             val env= KotlinCoreEnvironment.createForProduction(
@@ -98,7 +85,7 @@ class Scope2Code {
             var buffile=LightVirtualFile(scope.getFileName()!!, KotlinFileType.INSTANCE,renderer.render(scope))
             var psif=PsiManager.getInstance(env.project).findFile(buffile) as KtFile
             var parserkun = KotlinCodeVisitor(psif,VisualCodeBuilder_Impl())
-            parserkun.parse(psif)
+            parserkun.parse(psif)*/
         }
     }
 }
