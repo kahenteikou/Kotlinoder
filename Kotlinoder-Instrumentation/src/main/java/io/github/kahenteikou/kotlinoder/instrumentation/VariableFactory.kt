cb99e442444f088583863ffff3a2743e6a760ad4
kahenteikou <kokkiemouse@gmail.com>
2022-08-12 16:57:47 +0900

    x3

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VariableFactory.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/VariableFactory.kt
@@ -2,6 +2,9 @@ package io.github.kahenteikou.kotlinoder.instrumentation
 
 class VariableFactory {
     companion object{
-        
+        @JvmStatic
+        fun createObjectVariable(scope:Scope?,type:Type,varName:String):Variable{
+            return VariableImpl(scope,type,varName,null,false)
+        }
     }
 }
