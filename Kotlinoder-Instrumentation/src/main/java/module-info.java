361733d1a2b81f92310a2277d1f3bcf634781da8
kahenteikou <kokkiemouse@gmail.com>
2022-08-13 11:41:56 +0900

    s2

--- Kotlinoder-Instrumentation/src/main/java/module-info.java
+++ Kotlinoder-Instrumentation/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module io.github.kahenteikou.kotlinoder.instrumentation {
     requires kotlin.stdlib;
+    exports io.github.kahenteikou.kotlinoder.instrumentation;
+    exports io.github.kahenteikou.kotlinoder.lang;
     requires ast.jvm;
     requires ast.psi;
     requires java.logging;
-    exports io.github.kahenteikou.kotlinoder.instrumentation;
-    exports io.github.kahenteikou.kotlinoder.lang;
 }
