f7add88feb975c7dc0de33f6a3460f284f42cef4
kahenteikou <kokkiemouse@gmail.com>
2022-08-14 21:22:08 +0900

    name

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/ClassDeclarationRendererEx.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/ClassDeclarationRendererEx.kt
@@ -2,6 +2,7 @@ package io.github.kahenteikou.kotlinoder.instrumentation.renderers
 
 import io.github.kahenteikou.kotlinoder.instrumentation.ClassDeclaration
 import io.github.kahenteikou.kotlinoder.instrumentation.MethodDeclaration
+import io.github.kahenteikou.kotlinoder.instrumentation.Type
 import ktast.ast.Node
 
 class ClassDeclarationRendererEx:CodeRendererEx<ClassDeclaration, Node.Declaration.Class> {
@@ -14,7 +15,9 @@ class ClassDeclarationRendererEx:CodeRendererEx<ClassDeclaration, Node.Declarati
     override fun render(entity: ClassDeclaration): Node.Declaration.Class {
         lateinit var retClass:Node.Declaration.Class
         var bodykun:Node.Declaration.Class.Body?=null
-
+        var modifiers:Node.Modifiers?=null
+        var name: Node.Expression.Name= Node.Expression.Name(Type(entity.getName()).getShortName()!!)
+        
         return retClass
     }
     private fun createBody(e:ClassDeclaration):Node.Declaration.Class.Body?{
