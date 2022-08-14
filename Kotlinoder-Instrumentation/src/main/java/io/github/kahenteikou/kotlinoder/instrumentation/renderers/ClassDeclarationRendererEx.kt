f3ee97ef83fb031fa0da8eab3bbcaa68d2175bb6
kahenteikou <kokkiemouse@gmail.com>
2022-08-14 17:59:29 +0900

    s6

---
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/ClassDeclarationRendererEx.kt
@@ -0,0 +1,11 @@
+package io.github.kahenteikou.kotlinoder.instrumentation.renderers
+
+import io.github.kahenteikou.kotlinoder.instrumentation.ClassDeclaration
+import ktast.ast.Node
+
+class ClassDeclarationRendererEx:CodeRendererEx<ClassDeclaration, Node.Declaration> {
+    override fun render(entity: ClassDeclaration): Node.Declaration {
+        TODO("Not yet implemented")
+    }
+
+}
