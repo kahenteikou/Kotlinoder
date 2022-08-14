c3ef8a6ddb90cb37b72e76a5d501263e6538e4f9
kahenteikou <kokkiemouse@gmail.com>
2022-08-14 18:01:58 +0900

    s5

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/ClassDeclarationRendererEx.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/ClassDeclarationRendererEx.kt
@@ -4,14 +4,14 @@ import io.github.kahenteikou.kotlinoder.instrumentation.ClassDeclaration
 import io.github.kahenteikou.kotlinoder.instrumentation.MethodDeclaration
 import ktast.ast.Node
 
-class ClassDeclarationRendererEx:CodeRendererEx<ClassDeclaration, Node.Declaration> {
+class ClassDeclarationRendererEx:CodeRendererEx<ClassDeclaration, Node.Declaration.Class> {
     private var _methodDeclarationRenderer:CodeRendererEx<MethodDeclaration,Node.Declaration>?=null
     var methodDeclarationRenderer:CodeRendererEx<MethodDeclaration,Node.Declaration>
         get() = _methodDeclarationRenderer!!
         set(value) {
             _methodDeclarationRenderer = value
         }
-    override fun render(entity: ClassDeclaration): Node.Declaration {
+    override fun render(entity: ClassDeclaration): Node.Declaration.Class {
         TODO("Not yet implemented")
     }
 
