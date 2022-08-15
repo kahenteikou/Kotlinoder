5df9627ea06699423242cff895a367f2b3cc1551
kahenteikou <kokkiemouse@gmail.com>
2022-08-15 16:09:57 +0900

    6

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/MethodDeclarationRendererEx.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/MethodDeclarationRendererEx.kt
@@ -1,8 +1,9 @@
 package io.github.kahenteikou.kotlinoder.instrumentation.renderers
 
+import io.github.kahenteikou.kotlinoder.instrumentation.Invocation
 import io.github.kahenteikou.kotlinoder.instrumentation.MethodDeclaration
 import ktast.ast.Node
-
 class MethodDeclarationRendererEx :CodeRendererEx<MethodDeclaration, Node.Declaration.Function>{
+    private var _invocationRenderer:CodeRendererEx<Invocation,MutableList<Node.Statement>>?=null
     
 }
