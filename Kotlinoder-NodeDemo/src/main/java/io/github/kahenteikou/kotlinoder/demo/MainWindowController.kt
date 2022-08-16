877a90f8bcc3b4af0b0f27dd606b711cedd4de68
kahenteikou <kokkiemouse@gmail.com>
2022-08-17 08:49:57 +0900

    4g

--- Kotlinoder-NodeDemo/src/main/java/io/github/kahenteikou/kotlinoder/demo/MainWindowController.kt
+++ Kotlinoder-NodeDemo/src/main/java/io/github/kahenteikou/kotlinoder/demo/MainWindowController.kt
@@ -1,11 +1,28 @@
 package io.github.kahenteikou.kotlinoder.demo
 
+import eu.mihosoft.vrl.workflow.FlowFactory
+import eu.mihosoft.vrl.workflow.VFlow
+import eu.mihosoft.vrl.workflow.fx.ScalableContentPane
+import javafx.fxml.FXML
 import javafx.fxml.Initializable
+import javafx.scene.layout.Pane
 import java.net.URL
 import java.util.*
 
 class MainWindowController: Initializable {
+    @FXML
+    lateinit var view : Pane
+    lateinit private var rootPane: Pane
+    lateinit private var flow: VFlow
     override fun initialize(location: URL?, resources: ResourceBundle?) {
-        
+        println("init")
+        var canvas: ScalableContentPane = ScalableContentPane()
+        canvas.maxScaleX=1.0
+        canvas.maxScaleY=1.0
+        view.children.add(canvas)
+        var root : Pane = Pane()
+        canvas.content=root
+        rootPane=root
+        flow= FlowFactory.newFlow()
     }
 }
