c9a534b233527d75c6e82781ac565f5ad059994d
kahenteikou <kokkiemouse@gmail.com>
2022-08-17 08:41:11 +0900

    v

---
+++ Kotlinoder-NodeDemo/src/main/java/io/github/kahenteikou/kotlinoder/demo/Launcher.kt
@@ -0,0 +1,10 @@
+package io.github.kahenteikou.kotlinoder.demo
+
+class Launcher {
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>){
+            println("Hello, world!")
+        }
+    }
+}
