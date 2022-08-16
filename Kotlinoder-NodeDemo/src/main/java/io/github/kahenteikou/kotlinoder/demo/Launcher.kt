392702f64a8253eb004658fc8ab266988bfb98b7
kahenteikou <kokkiemouse@gmail.com>
2022-08-17 08:46:32 +0900

    666

--- Kotlinoder-NodeDemo/src/main/java/io/github/kahenteikou/kotlinoder/demo/Launcher.kt
+++ Kotlinoder-NodeDemo/src/main/java/io/github/kahenteikou/kotlinoder/demo/Launcher.kt
@@ -1,9 +1,14 @@
 package io.github.kahenteikou.kotlinoder.demo
 
 import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Node
+import javafx.scene.Scene
+import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 
 class Launcher :Application(){
+    lateinit private var controller : MainWindowController
     companion object{
         @JvmStatic
         fun main(args: Array<String>){
@@ -11,7 +16,20 @@ class Launcher :Application(){
         }
     }
 
-    override fun start(primaryStage: Stage?) {
+    override fun start(stage: Stage) {
         println("Hello, world!")
+        var loader = FXMLLoader(this.javaClass.getResource("MainWindow.fxml"))
+        var canvas = StackPane()
+        var scene = Scene(canvas, 800.0, 600.0)
+        try{
+            loader.load<Any>()
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        controller = loader.getController()
+        canvas.children.add((loader.getRoot() as Node))
+        stage.title = "MainWindow"
+        stage.scene=scene
+        stage.show()
     }
 }
