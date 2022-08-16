ccc08c50430fa8d7045742a43aa9be802eb41312
kahenteikou <kokkiemouse@gmail.com>
2022-08-17 08:44:38 +0900

    4

---
+++ Kotlinoder-NodeDemo/src/main/java/io/github/kahenteikou/kotlinoder/demo/MainWindowController.kt
@@ -0,0 +1,11 @@
+package io.github.kahenteikou.kotlinoder.demo
+
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.*
+
+class MainWindowController: Initializable {
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        
+    }
+}
