5b87de62d9ae03eadd34b71e1f69f309c23c0ab3
kahenteikou <kokkiemouse@gmail.com>
2022-08-18 07:19:32 +0900

    c6

---
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/invokes/PutValue_Impl.kt
@@ -0,0 +1,19 @@
+package io.github.kahenteikou.kotlinoder.instrumentation.invokes
+
+import io.github.kahenteikou.kotlinoder.instrumentation.Scope
+import io.github.kahenteikou.kotlinoder.instrumentation.Variable
+
+open class PutValue_Impl:PutValue {
+
+    private var _id : String
+    private final var parent: Scope
+    private var _SrcVariable: Variable
+    private var _DstVariable: Variable
+
+    constructor(parent:Scope,id:String,srcVar:Variable,dstVar:Variable){
+            this._id = id
+        this.parent = parent
+        this._SrcVariable = srcVar
+        this._DstVariable = dstVar
+    }
+}
