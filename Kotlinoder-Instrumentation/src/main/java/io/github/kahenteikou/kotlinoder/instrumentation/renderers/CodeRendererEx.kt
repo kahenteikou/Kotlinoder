7ff7a792689a4d0a4c3a6d8dd930490b367644ba
kahenteikou <kokkiemouse@gmail.com>
2022-08-20 07:20:35 +0900

    6

--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/CodeRendererEx.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/CodeRendererEx.kt
@@ -4,7 +4,7 @@ import io.github.kahenteikou.kotlinoder.instrumentation.CodeBuilder
 import io.github.kahenteikou.kotlinoder.instrumentation.CodeEntity
 import ktast.ast.Node
 
-interface CodeRendererEx <T:CodeEntity,T2:Node>{
+interface CodeRendererEx <T,T2:Node>{
     fun render(entity: T): T2
     //fun render(entity:T,cb: CodeBuilder)
 }
--- Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/PutValueRenderer.kt
+++ Kotlinoder-Instrumentation/src/main/java/io/github/kahenteikou/kotlinoder/instrumentation/renderers/PutValueRenderer.kt
@@ -1,4 +1,11 @@
 package io.github.kahenteikou.kotlinoder.instrumentation.renderers
 
-open class PutValueRenderer {
+import io.github.kahenteikou.kotlinoder.instrumentation.invokes.PutValue
+import ktast.ast.Node
+
+open class PutValueRenderer:CodeRendererEx<PutValue, Node.Statement> {
+    override fun render(entity: PutValue): Node.Statement {
+        TODO("Not yet implemented")
+    }
+
 }
