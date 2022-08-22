3567660ac45b7c95f30b8b116926c389aae59b3e
kahenteikou <kokkiemouse@gmail.com>
2022-08-23 07:09:06 +0900

    3

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/MainWindowController.kt
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/MainWindowController.kt
@@ -1,5 +1,7 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import java.net.URL
 import java.util.*
@@ -8,5 +10,22 @@ class MainWindowController : Initializable {
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         println("Start!")
     }
+    @FXML
+    fun onLoadAction(e:ActionEvent){
+
+    }
+    @FXML
+    fun onSaveAction(e:ActionEvent){
+
+    }
+    @FXML
+    fun onSaveAsAction(e:ActionEvent){
+
+    }
+    @FXML
+    fun onCloseAction(e:ActionEvent){
+
+    }
+
 
 }
