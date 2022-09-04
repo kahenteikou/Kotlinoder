3fb82b2263eecbae0c14a9c20abf118b7ef3595d
kahenteikou <kokkiemouse@gmail.com>
2022-09-04 16:01:40 +0900

    v2

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/ClassEditorTabController.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/ClassEditorTabController.java
@@ -1,12 +1,16 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main.tabs;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import org.apache.logging.log4j.LogManager;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ClassEditorTabController implements Initializable {
+    @FXML
+    private TextField ClassNameTxtField;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         LogManager.getLogger("ClassEditorTabController").info("Init");
