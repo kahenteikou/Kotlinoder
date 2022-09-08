3d2421ed66cb790bd6dbabe6d536f2fedf147b74
kahenteikou <kokkiemouse@gmail.com>
2022-09-08 14:54:08 +0900

    f2

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/FileTreeWrappedItem.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/FileTreeWrappedItem.java
@@ -5,7 +5,7 @@ import java.io.File;
 
 public class FileTreeWrappedItem extends TreeWrappedItem {
 
-    public FileTreeWrappedItem(String title, TreeWrappedItem type) {
+    public FileTreeWrappedItem(String title, TreeWrappedItemType type) {
         super(title, type);
     }
     public FileTreeWrappedItem(File _f){
