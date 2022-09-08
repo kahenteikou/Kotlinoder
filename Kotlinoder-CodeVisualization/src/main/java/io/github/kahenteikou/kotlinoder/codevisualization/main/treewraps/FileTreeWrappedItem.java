622fec7f91f720d0e5eb5942eabaafc588a28d20
kahenteikou <kokkiemouse@gmail.com>
2022-09-08 14:51:33 +0900

    c2

---
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/FileTreeWrappedItem.java
@@ -0,0 +1,9 @@
+package io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps;
+
+
+public class FileTreeWrappedItem extends TreeWrappedItem {
+
+    public FileTreeWrappedItem(String title, TreeWrappedItem type) {
+        super(title, type);
+    }
+}
