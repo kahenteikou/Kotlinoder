d3de6bad5c68dcea4d78aed13eb848f0740e8a65
kahenteikou <kokkiemouse@gmail.com>
2022-09-08 14:45:53 +0900

    6c

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/TreeWrappedItem.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/TreeWrappedItem.java
@@ -3,6 +3,25 @@ package io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps;
 public class TreeWrappedItem {
     private String _title;
     private Object _data;
+    private TreeWrappedItem _type;
+    public String getTitle(){
+        return _title;
+    }
+    public Object getData(){
+        return _data;
+    }
+    public TreeWrappedItem getType(){
+        return _type;
+    }
+    public void setTitle(String title){
+        _title = title;
+    }
+    public void setData(Object dt){
+        _data=dt;
+    }
+    public void setType(TreeWrappedItem type){
+        _type=type;
+    }
     public enum TreeWrappedItemType{
         CLASS,
         METHOD,
