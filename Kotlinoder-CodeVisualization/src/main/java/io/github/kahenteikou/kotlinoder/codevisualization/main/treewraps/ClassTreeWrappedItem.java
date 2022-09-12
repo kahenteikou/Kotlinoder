cd00259d8efb13c0355dbec5db0354023d2f06fe
kahenteikou <kokkiemouse@gmail.com>
2022-09-13 07:18:39 +0900

    7

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/ClassTreeWrappedItem.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/ClassTreeWrappedItem.java
@@ -4,6 +4,6 @@ import io.github.kahenteikou.kotlinoder.instrumentation.ClassDeclaration;
 
 public class ClassTreeWrappedItem extends TreeWrappedItem {
     public ClassTreeWrappedItem(ClassDeclaration clsd){
-        super(title, TreeWrappedItemType.CLASS);
+        super(clsd.getName(), TreeWrappedItemType.CLASS);
     }
 }
