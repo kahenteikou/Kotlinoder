3f78d6c54b071eb05f2e56383f088349865504a0
kahenteikou <kokkiemouse@gmail.com>
2022-09-15 07:19:51 +0900

    c

---
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treeviews/FileTreeCellImpl.java
@@ -0,0 +1,8 @@
+package io.github.kahenteikou.kotlinoder.codevisualization.main.treeviews;
+
+import io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps.TreeWrappedItem;
+import javafx.scene.control.TreeCell;
+
+public class FileTreeCellImpl extends TreeCell<TreeWrappedItem> {
+
+}
