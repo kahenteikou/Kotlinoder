2dc8465b929389b097191c9ea4259670846d5f59
kahenteikou <kokkiemouse@gmail.com>
2022-09-15 23:37:03 +0900

    cz

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/TreeitemWs/TreeItem_ClassTreeWrappedItem.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/TreeitemWs/TreeItem_ClassTreeWrappedItem.java
@@ -2,6 +2,12 @@ package io.github.kahenteikou.kotlinoder.codevisualization.main.TreeitemWs;
 
 import io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps.TreeWrappedItem;
 import javafx.scene.control.TreeItem;
+import javafx.scene.image.Image;
 
 public class TreeItem_ClassTreeWrappedItem extends TreeItem<TreeWrappedItem> {
+
+    private static final Image ClsImage=loadImage("clsicon.png");
+    private static Image loadImage(String filepath){
+        return new Image(TreeItem_ClassTreeWrappedItem.class.getResourceAsStream(filepath));
+    }
 }
