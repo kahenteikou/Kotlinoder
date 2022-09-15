11ff695ef73c0ea38184940e4d7ac5515fdb7b7f
kahenteikou <kokkiemouse@gmail.com>
2022-09-16 07:10:09 +0900

    x

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/TreeitemWs/TreeItem_MethodTreeWrappedItem.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/TreeitemWs/TreeItem_MethodTreeWrappedItem.java
@@ -3,10 +3,14 @@ package io.github.kahenteikou.kotlinoder.codevisualization.main.TreeitemWs;
 import io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps.ClassTreeWrappedItem;
 import io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps.MethodTreeWrappedItem;
 import io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps.TreeWrappedItem;
+import io.github.kahenteikou.kotlinoder.instrumentation.MethodDeclaration;
 import javafx.scene.control.TreeItem;
 
 public class TreeItem_MethodTreeWrappedItem extends TreeItem<TreeWrappedItem> {
     public TreeItem_MethodTreeWrappedItem(MethodTreeWrappedItem value) {
         super(value);
+    }
+    public TreeItem_MethodTreeWrappedItem(MethodDeclaration md){
+        super(new MethodTreeWrappedItem(md));
     }
 }
