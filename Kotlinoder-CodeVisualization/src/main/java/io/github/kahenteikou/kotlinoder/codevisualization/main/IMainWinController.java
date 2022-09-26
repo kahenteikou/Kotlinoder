b4e0a97d99ad6002848eb09015741e94398f36b8
kahenteikou <kokkiemouse@gmail.com>
2022-09-26 23:42:19 +0900

    added 2

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/IMainWinController.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/IMainWinController.java
@@ -3,5 +3,5 @@ package io.github.kahenteikou.kotlinoder.codevisualization.main;
 import javafx.scene.control.Tab;
 
 public interface IMainWinController {
-    public void add_tab(Tab tab);
+    public void add_tab(Tab tab,String tab_id);
 }
