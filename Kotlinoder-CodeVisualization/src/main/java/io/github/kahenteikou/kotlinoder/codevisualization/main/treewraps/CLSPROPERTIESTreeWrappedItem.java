f48164feb3bd601f7ac457b28c75be1a61e6eb1e
kahenteikou <kokkiemouse@gmail.com>
2022-09-27 00:15:29 +0900

    S

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/CLSPROPERTIESTreeWrappedItem.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/CLSPROPERTIESTreeWrappedItem.java
@@ -1,7 +1,10 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps;
 
 import io.github.kahenteikou.kotlinoder.codevisualization.main.IMainWinController;
+import io.github.kahenteikou.kotlinoder.codevisualization.main.tabs.STUBCLS;
 import io.github.kahenteikou.kotlinoder.instrumentation.ClassDeclaration;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Tab;
 
 public class CLSPROPERTIESTreeWrappedItem extends TreeWrappedItem{
     private ClassDeclaration cd;
@@ -22,6 +25,14 @@ public class CLSPROPERTIESTreeWrappedItem extends TreeWrappedItem{
     @Override
     public void onDoubleClick(IMainWinController controller) {
         System.out.println("Double clicked CLSProp II");
-        
+        FXMLLoader loader=new FXMLLoader(new STUBCLS().getClass().getResource("ClassEditorTab.fxml"));
+        try{
+            loader.load();
+            Tab tab=new Tab("PROP");
+            tab.setContent(loader.getRoot());
+            controller.add_tab(tab,String.format("PROP_%s_%s",cd.getName(),cd.getId()));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
