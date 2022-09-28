4d59e1b820efab05fd1615dc68264181b2b0462d
kahenteikou <kokkiemouse@gmail.com>
2022-09-29 07:18:15 +0900

    back

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/METHODPROPERTIESTreeWrappedItem.java
+++
@@ -1,7 +0,0 @@
-package io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps;
-
-public class METHODPROPERTIESTreeWrappedItem extends TreeWrappedItem{
-    public METHODPROPERTIESTreeWrappedItem(String title, TreeWrappedItemType type) {
-        super(title, type);
-    }
-}
