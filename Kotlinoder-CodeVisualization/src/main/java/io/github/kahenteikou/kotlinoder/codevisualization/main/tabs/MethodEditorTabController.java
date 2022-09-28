3ae14409ff884bc74cb82793926dac866c52450e
kahenteikou <kokkiemouse@gmail.com>
2022-09-29 07:23:03 +0900

    6

---
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/MethodEditorTabController.java
@@ -0,0 +1,13 @@
+package io.github.kahenteikou.kotlinoder.codevisualization.main.tabs;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MethodEditorTabController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
