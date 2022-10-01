3ac3815ff487d871807af58d1fc7ed21867e5f58
kahenteikou <kokkiemouse@gmail.com>
2022-10-01 13:26:25 +0900

    4

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/MethodTreeWrappedItem.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/treewraps/MethodTreeWrappedItem.java
@@ -1,7 +1,12 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main.treewraps;
 
 import io.github.kahenteikou.kotlinoder.codevisualization.main.IMainWinController;
+import io.github.kahenteikou.kotlinoder.codevisualization.main.tabs.ClassEditorTabController;
+import io.github.kahenteikou.kotlinoder.codevisualization.main.tabs.MethodEditorTabController;
+import io.github.kahenteikou.kotlinoder.codevisualization.main.tabs.STUBCLS;
 import io.github.kahenteikou.kotlinoder.instrumentation.MethodDeclaration;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Tab;
 
 public class MethodTreeWrappedItem extends TreeWrappedItem{
     private MethodDeclaration md;
@@ -16,5 +21,17 @@ public class MethodTreeWrappedItem extends TreeWrappedItem{
     @Override
     public void onDoubleClick(IMainWinController controller) {
         System.out.println("Double clicked method II");
+        FXMLLoader loader=new FXMLLoader(new STUBCLS().getClass().getResource("MethodEditorTab.fxml"));
+        try{
+            loader.load();
+            Tab tab=new Tab("Method");
+            tab.setContent(loader.getRoot());
+            MethodEditorTabController tabCon=loader.getController();
+            //tabCon.setClsInfo(cd);
+            controller.add_tab(tab,String.format("PROP_%s",md.getName()));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
