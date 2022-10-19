cd0de6b2d30c9cfd5e75bca8a5af5e3383c15621
kahenteikou <kokkiemouse@gmail.com>
2022-10-19 10:55:45 +0900

    E7

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/MethodEditorTabController.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/MethodEditorTabController.java
@@ -21,8 +21,13 @@ public class MethodEditorTabController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
         LogManager.getLogger("MethodEditorTabController").info("Init");
-        ScalableContentPane scalableContentPane = new ScalableContentPane();
-        
+        ScalableContentPane canvas = new ScalableContentPane();
+
+        canvas.setStyle("-fx-background-color: rgb(0,0,0)");
+        canvas.setMaxScaleX(1.0);
+        canvas.setMaxScaleY(1.0);
+        canvas.setMinScaleX(1.0);
+        canvas.setMinScaleY(1.0);
     }
     public void setMethodinfo(MethodDeclaration md){
         this.md=md;
