1028f386046477a1d87230ccfed50ded35294771
kahenteikou <kokkiemouse@gmail.com>
2022-10-20 07:21:23 +0900

    E1

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/MethodEditorTabController.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/MethodEditorTabController.java
@@ -8,6 +8,7 @@ import eu.mihosoft.vrl.workflow.fx.ScalableContentPane;
 import io.github.kahenteikou.kotlinoder.instrumentation.MethodDeclaration;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 import org.apache.logging.log4j.LogManager;
 
@@ -24,13 +25,15 @@ public class MethodEditorTabController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
         LogManager.getLogger("MethodEditorTabController").info("Init");
-        ScalableContentPane canvas = new ScalableContentPane();
+        /*ScalableContentPane canvas = new ScalableContentPane();
 
         canvas.setStyle("-fx-background-color: rgb(0,0,0)");
         canvas.setMaxScaleX(1.0);
         canvas.setMaxScaleY(1.0);
         canvas.setMinScaleX(1.0);
         canvas.setMinScaleY(1.0);
+        view.getChildren().add(canvas);*/
+        ScrollPane canvas=new ScrollPane();
         view.getChildren().add(canvas);
         Pane root=new Pane();
         canvas.setContent(root);
