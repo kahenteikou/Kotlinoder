55a16e14b80175edbd45bcbf3adade026e71cb34
kahenteikou <kokkiemouse@gmail.com>
2022-10-19 10:49:41 +0900

    E

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/MethodEditorTabController.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/MethodEditorTabController.java
@@ -1,7 +1,9 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main.tabs;
 
 import io.github.kahenteikou.kotlinoder.instrumentation.MethodDeclaration;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 import org.apache.logging.log4j.LogManager;
 
 import java.net.URL;
@@ -9,6 +11,8 @@ import java.util.ResourceBundle;
 
 public class MethodEditorTabController implements Initializable {
     private MethodDeclaration md;
+    @FXML
+    private Pane view;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
