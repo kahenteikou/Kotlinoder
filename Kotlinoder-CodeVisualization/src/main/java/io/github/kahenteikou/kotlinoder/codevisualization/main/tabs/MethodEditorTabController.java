62a28043b341dd1eaf0018f1df5b9ac7b06290fb
kahenteikou <kokkiemouse@gmail.com>
2022-10-24 14:38:34 +0900

    4

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/MethodEditorTabController.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/tabs/MethodEditorTabController.java
@@ -80,13 +80,13 @@ public class MethodEditorTabController implements Initializable {
 
     }
     private VFlow scopeToFlow(Scope scope, VFlow parent){
-        VFlow resultFlow=parent.newSubFlow();
+        /*VFlow resultFlow=parent.newSubFlow();
         FXValueSkinFactory fXSkinFactory = new FXValueSkinFactory(rootPane);
-        resultFlow.setSkinFactories(fXSkinFactory);
-        invocationNodes.put(scope,resultFlow.getModel());
-        String title=String.format("%s %s(): %s",scope.getType(),scope.getName(),scope.getId());
-        resultFlow.getModel().setTitle(title);
+        resultFlow.setSkinFactories(fXSkinFactory);*/
+        invocationNodes.put(scope,parent.getModel());
+        String title=String.format("Entry Point %s %s(): %s",scope.getType(),scope.getName(),scope.getId());
+        parent.getModel().setTitle(title);
 
-        return resultFlow;
+        return parent;
     }
 }
