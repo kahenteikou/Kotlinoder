0a38dcb0ff90592b3ae353849fc959b59972c6da
kahenteikou <kokkiemouse@gmail.com>
2022-11-05 07:14:53 +0900

    z9

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/nodevaluesystem/methods/MethodCallValue.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/nodevaluesystem/methods/MethodCallValue.java
@@ -1,4 +1,11 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main.nodevaluesystem.methods;
 
 public class MethodCallValue {
+    private String _targetMethod_name;
+    public String getTargetMethodName(){
+        return _targetMethod_name;
+    }
+    public void setTargetMethodName(String _targetMethod_name) {
+        this._targetMethod_name = _targetMethod_name;
+    }
 }
