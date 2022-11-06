81afa8fe3a701751fd2149fe4fb92543c2a56376
kahenteikou <kokkiemouse@gmail.com>
2022-11-06 21:59:52 +0900

    X2

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/NodeSkinBase.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/NodeSkinBase.java
@@ -1,9 +1,12 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main.skins;
 
 import eu.mihosoft.vrl.workflow.VFlow;
+import eu.mihosoft.vrl.workflow.VFlowModel;
 import eu.mihosoft.vrl.workflow.VNode;
 import eu.mihosoft.vrl.workflow.fx.FXFlowNodeSkinBase;
 import eu.mihosoft.vrl.workflow.fx.FXSkinFactory;
+import eu.mihosoft.vrl.workflow.fx.ScalableContentPane;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 
 public abstract class NodeSkinBase extends FXFlowNodeSkinBase {
@@ -15,5 +18,17 @@ public abstract class NodeSkinBase extends FXFlowNodeSkinBase {
     @Override
     public void updateView() {
         super.updateView();
+        if(getModel() instanceof VFlowModel){
+            return;
+        }
+        if(getModel().getValueObject().getValue()==null){
+            return;
+        }
+        Node view=CreateView();
+        if(view!=null){
+            ScalableContentPane spane=new ScalableContentPane();
+            spane.setPadding(new Insets(10));
+            
+        }
     }
 }
