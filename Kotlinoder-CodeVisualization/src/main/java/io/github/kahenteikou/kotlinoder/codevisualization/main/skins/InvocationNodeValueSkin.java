6b79176e56233bde711a956c837b45c123dd9e66
kahenteikou <kokkiemouse@gmail.com>
2022-11-06 22:13:22 +0900

    K2

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/InvocationNodeValueSkin.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/InvocationNodeValueSkin.java
@@ -3,8 +3,10 @@ package io.github.kahenteikou.kotlinoder.codevisualization.main.skins;
 import eu.mihosoft.vrl.workflow.VFlow;
 import eu.mihosoft.vrl.workflow.VNode;
 import eu.mihosoft.vrl.workflow.fx.FXSkinFactory;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
 public class InvocationNodeValueSkin extends NodeSkinBase{
     public InvocationNodeValueSkin(FXSkinFactory skinFactory, VNode model, VFlow controller) {
@@ -13,6 +15,9 @@ public class InvocationNodeValueSkin extends NodeSkinBase{
 
     @Override
     protected Node CreateView() {
-        return new Label("Hello World");
+        Label retLabel=new Label("Hello World");
+        retLabel.setFont(new Font(30));
+        retLabel.setAlignment(Pos.CENTER);
+        return retLabel;
     }
 }
