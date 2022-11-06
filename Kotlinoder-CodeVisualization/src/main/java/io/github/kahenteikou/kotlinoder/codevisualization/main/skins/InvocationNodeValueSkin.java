13a6ae69604f76b130b6f615f1c1b05406af5ab0
kahenteikou <kokkiemouse@gmail.com>
2022-11-06 22:03:41 +0900

    X

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/InvocationNodeValueSkin.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/InvocationNodeValueSkin.java
@@ -1,4 +1,18 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main.skins;
 
+import eu.mihosoft.vrl.workflow.VFlow;
+import eu.mihosoft.vrl.workflow.VNode;
+import eu.mihosoft.vrl.workflow.fx.FXSkinFactory;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+
 public class InvocationNodeValueSkin extends NodeSkinBase{
+    public InvocationNodeValueSkin(FXSkinFactory skinFactory, VNode model, VFlow controller) {
+        super(skinFactory, model, controller);
+    }
+
+    @Override
+    protected Node CreateView() {
+        return new Label("Hello World");
+    }
 }
