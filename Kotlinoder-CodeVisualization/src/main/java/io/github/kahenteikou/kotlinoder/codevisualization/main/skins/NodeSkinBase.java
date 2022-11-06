ad8e92c1f3e490ece7d8cf849ad143560e2dfd00
kahenteikou <kokkiemouse@gmail.com>
2022-11-06 21:56:35 +0900

    56

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/NodeSkinBase.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/NodeSkinBase.java
@@ -1,6 +1,13 @@
 package io.github.kahenteikou.kotlinoder.codevisualization.main.skins;
 
+import eu.mihosoft.vrl.workflow.VFlow;
+import eu.mihosoft.vrl.workflow.VNode;
 import eu.mihosoft.vrl.workflow.fx.FXFlowNodeSkinBase;
+import eu.mihosoft.vrl.workflow.fx.FXSkinFactory;
 
 public abstract class NodeSkinBase extends FXFlowNodeSkinBase {
+    public NodeSkinBase(FXSkinFactory skinFactory,
+                        VNode model, VFlow controller){
+        super(skinFactory, model, controller);
+    }
 }
