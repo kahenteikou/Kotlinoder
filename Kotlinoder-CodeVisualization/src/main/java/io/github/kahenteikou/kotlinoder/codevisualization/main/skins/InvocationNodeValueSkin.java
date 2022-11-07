21ed899618bca66ad26911e0d8556dde3796690d
kahenteikou <kokkiemouse@gmail.com>
2022-11-08 07:10:59 +0900

    2

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/InvocationNodeValueSkin.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/InvocationNodeValueSkin.java
@@ -13,11 +13,12 @@ import org.apache.logging.log4j.LogManager;
 public class InvocationNodeValueSkin extends NodeSkinBase{
     public InvocationNodeValueSkin(FXSkinFactory skinFactory, VNode model, VFlow controller) {
         super(skinFactory, model, controller);
-        LogManager.getLogger().info("InvocationNodeValueSkin new");
     }
 
     @Override
     protected Node CreateView() {
+        LogManager.getLogger().info("InvocationNodeValueSkin CreateView!");
+        
         Label retLabel=new Label(((Invocation)(getModel().getValueObject().getValue())).getVariableName());
         retLabel.setFont(new Font(30));
         retLabel.setAlignment(Pos.CENTER);
