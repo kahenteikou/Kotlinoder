ea75966da5036292e745729a0629823ed16dcd35
kahenteikou <kokkiemouse@gmail.com>
2022-11-08 07:12:32 +0900

    L2

--- Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/InvocationNodeValueSkin.java
+++ Kotlinoder-CodeVisualization/src/main/java/io/github/kahenteikou/kotlinoder/codevisualization/main/skins/InvocationNodeValueSkin.java
@@ -3,7 +3,9 @@ package io.github.kahenteikou.kotlinoder.codevisualization.main.skins;
 import eu.mihosoft.vrl.workflow.VFlow;
 import eu.mihosoft.vrl.workflow.VNode;
 import eu.mihosoft.vrl.workflow.fx.FXSkinFactory;
+import io.github.kahenteikou.kotlinoder.codevisualization.main.tabs.STUBCLS;
 import io.github.kahenteikou.kotlinoder.instrumentation.Invocation;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -19,6 +21,8 @@ public class InvocationNodeValueSkin extends NodeSkinBase{
     protected Node CreateView() {
         LogManager.getLogger().info("InvocationNodeValueSkin CreateView!");
 
+        FXMLLoader loader=new FXMLLoader(this.getClass().getResource("InvocationNodeValuekun.fxml"));
+        
         Label retLabel=new Label(((Invocation)(getModel().getValueObject().getValue())).getVariableName());
         retLabel.setFont(new Font(30));
         retLabel.setAlignment(Pos.CENTER);
